6f610dd4eb00d29c9b17c356fbe6f79097e7e7ec
xiatianyang <0049002606@znv.com>
2019-06-10 11:52:25 +0800

    add axios

--- src/main.js
+++ src/main.js
@@ -1,12 +1,15 @@
 import Vue from 'vue'
+import './plugins/axios'
 import App from './App.vue'
 import router from './router'
-import iView from 'iview';
-import 'iview/dist/styles/iview.css';
+import store from './store'
+import iView from 'iview'
+import 'iview/dist/styles/iview.css'
+import axios from './util/axios'
 import store from './store'
 Vue.use(iView);
 Vue.config.productionTip = false
-
+Vue.prototype.axios = axios
 new Vue({
   router,
   store,
---
+++ src/utils/request.js
@@ -0,0 +1,42 @@
+import axios from 'axios'
+import store from '@/store'
+import httpCode from './httpCode'
+
+const requestService = axios.create({
+  baseURL: '/DVAAP/v1',
+  timeout: 600000
+})
+
+let requestTimes = 0
+requestService.interceptors.request.use(
+  config => {
+    store.commit('showLoading', true)
+    ++requestTimes
+    return config
+  },
+  error => {
+    store.commit('showLoading', false)
+    --requestTimes
+    console.error('[request error]: ', error)
+    Promise.reject(error)
+  }
+)
+
+requestService.interceptors.response.use(
+  response => {
+    !--requestTimes && store.commit('showLoading', false)
+    if (response.data && response.data.code === 200) {
+      return response.data.data
+    }else {
+      return Promise.reject("出现错误，请检查算子流以及算子流配置信息！")
+    }
+
+   // return Promise.reject(response.data.message)
+  },
+  error => {
+    !--requestTimes && store.commit('showLoading', false)
+    return Promise.reject(error)
+  }
+)
+
+export default requestService
