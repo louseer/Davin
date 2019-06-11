4160030ce349cfcc45cee0089b48c8cd10eb2e33
xiatianyang <0049002606@znv.com>
2019-06-11 09:27:43 +0800

    addClass

--- src/pages/domcanvas/domcanvas.js
+++ src/pages/domcanvas/domcanvas.js
@@ -1,12 +1,82 @@
 
-export class DomCavase {
-  constructor(el,mode){
-    this.el=el
-    this.mode=mode
+export default class DomCavase {
+  constructor(el, mode ,grid=[5,5]) {
+    this.el = el    
+    this.mode = mode || 'select'   
+    this.grid=grid
   }
-  creatNode(node,type){
-    return{
-      //XXXXX
+  set canvasZoom(val){    
+    this.el.style.transform=`scale(${val},${val})`
+  }
+  get canvasZoom(){    
+    const arr=this.el.style.transform.toString().split(',')
+    if(arr.length ===0){
+    const num = arr[1].substring(0,arr[1].length-1)   
+    return  `${+num*100}%`
     }
+  }
+  set canvasOpcity(val) {    
+    this.el.style.opacity = val
+  }
+  get canvasOpcity(){
+    return getStyle(this.el, 'opacity')
+  }
+  set canvasWidth(val){  
+    this.el.style.width=`${val}px`
+  }
+  get canvasWidth(){
+   return  this.el.offsetWidth
+  }
+  set canvasHeight(val){  
+    this.el.style.height=`${val}px`
+  }
+  get canvasHeight(){
+   return  this.el.offsetHeight
+  }
+  set canvasBackColor(val){
+    this.el.style.backgroudColor=`#${val}`
+  }
+  get canvasBackColor(){
+   return getStyle(this.el,'backgroundColor')
+  }
+  setImg(url){
+    this.el.style.backgroud=`${url}`
+  }
+  canvasOnclick(callback) {
+    this.el.onmousedown = (e) => { 
+     const event = e || window.event   
+      callback && typeof callback ==='function' && callback(event)
+      if(event.button == "2"){  
+      alert("画布右击")
+}
+    }
+  }
+
+  createNode(ele) {
+    return {
+      elementType: ele.elementType || 'node',
+      x: ele.x,
+      y: ele.y,
+      type: ele.type || '',
+      id: ele.id || getuuid(),
+      iconUrl: ele.iconUrl || '',
+      isSwich: ele.isSwich === undefined ? true:ele.isSwich ,
+      nodeBgin: ele.nodeBgin === undefined ? null :  ele.nodeBgin.id ,
+      nodeEnd: ele.nodeEnd === undefined ? null :  ele.nodeEnd.id ,
+      name: ele.text || '',
+      childs: ele.childs || null,
+      isServiceNode: ele.isServiceNode || '-1',
+      isPublish: ele.isPublish || '-1',
+      startNodes: ele.startNodes || null,
+      endNodes: ele.endNodes || null,
+      category: ele.category || ''
+    }
+  }
+}
+function getStyle(obj, styleName) {
+  if (obj.currentStyle) {
+    return obj.currentStyle[styleName]
+  } else {
+    return getComputedStyle(obj, null)[styleName]
   }
 }
--- src/pages/domcanvas/index.vue
+++ src/pages/domcanvas/index.vue
@@ -1,29 +1,72 @@
 <template>
-  <div>
+  <div style="overflow: hidden;position: relative; ">
+    <input v-model="width" type="text" placeholder="请输入宽度">
+    <input v-model="opacity" type="text" placeholder="请输入透明度">
     <h1>画布在下方⬇</h1>
-    <div class="stage">
-      <div class="canvas"></div>
+
+    <div class="stage" ref="stage">
+      <div class="canvas" ref="canvas"></div>
     </div>
   </div>
 </template>
 
 <script>
-  export default {
-    
+import domCavase from "././domcanvas";
+export default {
+  data() {
+    return {
+      width: "",
+      domCavase: {},
+      opacity: ""
+    };
+  },
+  
+  watch: {
+    opacity(val) {
+      this.domCavase.canvasOpcity = +val;
+    },
+    width(val) {
+      this.domCavase.canvasWidth = +val;
+    }
+  },
+  mounted() {
+    const stage = this.$refs.stage;
+    const canvas = this.$refs.canvas;
+    this.domCavase = new domCavase(canvas);
+    console.log(this.domCavase);
+
+    this.domCavase.canvasOnclick(e => {
+      console.log(e.screenX, e.screenY);
+    });
+
+    console.log(this.domCavase.canvasWidth);
+   
+  },
+  methods: {
+    name() {}
   }
+};
 </script>
 
 <style scoped lang='less'>
-.stage{
+input {
+  border: 1px #dddddd solid;
+  height: 40px;
+  width: 300px;
+  // float: left;
+  margin-right: 20px;
+  float: left;
+}
+.stage {
   width: 100%;
   height: 800px;
-  background: darkgray;
-  position: absolute;
-  .canvas{
+  background: black;
+  position: relative;
+  .canvas {
     width: 90%;
     height: 90%;
     background: gray;
-    margin: auto
+    margin: auto;
   }
 }
 </style>
