a2e0404f9a3f2e36343c0af1a3e2ebe22e101d43
qinxy18 <qinxy18@aliyun.com>
2019-07-29 10:33:29 +0800

    极坐标需引入其他类

--- src/pages/edit2/chart/vchart/vchart.vue
+++ src/pages/edit2/chart/vchart/vchart.vue
@@ -6,8 +6,8 @@
 <script>
 //import {组件名称} from '组件路径';
 import ECharts from 'vue-echarts'
-// import 'echarts/lib/chart/line'
-// import 'echarts/lib/component/polar'
+import 'echarts/lib/chart/line'
+import 'echarts/lib/component/polar'
 // import theme from '../theme.json' //引入主题
 // ECharts.registerTheme('ovilia-green', theme); //引入主题
 import { getChartData } from '@/api/api.js'
@@ -25,67 +25,20 @@ export default {
 
     }
   },
+  data () {
+    return {
+      instance:null,
+      chartData:null,
+      options:'',
+      ready:false
+    }
+  },
   computed: {
     type () {  
       return this.config.type;
     },
     apiUrl () {
       return this.config.data.apiUrl;
-    },
-    // options () {
-    //   if(this.ready){
-    //     return this.instance.options
-    //   }else{
-    //     return {}
-    //   }
-    // }
-  },
-  data () {
-    // let data = []
-    // for (let i = 0; i <= 360; i++) {
-    //     let t = i / 180 * Math.PI
-    //     let r = Math.sin(2 * t) * Math.cos(2 * t)
-    //     data.push([r, i])
-    // }
-    // console.log(this.instance)
-    // return {
-    //   polar:{
-    //     legend: {
-    //       data: ['line']
-    //     },
-    //     polar: {
-    //       center: ['50%', '54%']
-    //     },
-    //     tooltip: {
-    //       trigger: 'axis',
-    //       axisPointer: {
-    //         type: 'cross'
-    //       }
-    //     },
-    //     angleAxis: {
-    //       type: 'value',
-    //       startAngle: 0
-    //     },
-    //     radiusAxis: {
-    //       min: 0
-    //     },
-    //     series: [
-    //       {
-    //         coordinateSystem: 'polar',
-    //         name: 'line',
-    //         type: 'line',
-    //         showSymbol: false,
-    //         data: data
-    //       }
-    //     ],
-    //     animationDuration: 2000
-    //   }
-    // }
-    return {
-      instance:null,
-      chartData:null,
-      options:'',
-      ready:false
     }
   },
   watch: {
